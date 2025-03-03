636863ca097a33c50a6671f2631195abbc1485e7
Sjors Verhoef <sjors_verhoef@icloud.com>
2025-03-03 14:54:38 +0100

    seperate tests

--- frontend/src/__tests__/App.spec.tsx
+++ frontend/src/__tests__/App.spec.tsx
@@ -1,17 +1,9 @@
 import '@testing-library/jest-dom'
 import {render} from '@testing-library/react';
 import App from '../App';
-import {decodeHtml} from "../utils/utils";
 import {act} from "react";
 
 describe('App Component', () => {
-
-    describe("decodeHtml", () => {
-        it('decodes special html characters', () => {
-            expect(decodeHtml("What was Genghis Khan&#039;s real name?")).toBe("What was Genghis Khan's real name?")
-        })
-    })
-
     it('renders correctly', async () => {
         await act(() => render(<App />))
     })
